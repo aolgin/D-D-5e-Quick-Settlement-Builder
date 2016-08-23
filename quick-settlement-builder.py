84977d9c01fff20af8dc5b304f8635071f3f8e63
Adam Olgin <aolgin@ccs.neu.edu>
2016-08-23 16:58:34 -0400

    Name and feature tables added, proper printing and string formatting in progress

--- quick-settlement-builder.py
+++ quick-settlement-builder.py
@@ -4,15 +4,15 @@ import roll_tables as tables
 
 MAX_FEATURES = 4
 
-class settlement:
+class Settlement:
 	# Filled constructor
-	def __init__(self, name="", size="", population=0, location="", 
+	def __init__(self, name="", size="", features=[], population=0, location="", 
 		purpose="", resources=[], races=[], gods=[]):
 		self.name = name
 		self.size = size
-		self.population = population
+		self.features = features
 		self.location = location
-		#self.features = features
+		self.population = population
 		self.purpose = purpose
 		self.resources = resources
 		self.races = races
@@ -22,17 +22,28 @@ class settlement:
 	def roll(self, num_sides, start=1, debug=False):
 		return random.randint(start, num_sides)
 
+	def roll_name(self, debug=False):
+		first = tables.first_name_element[self.roll(len(tables.first_name_element))]
+		second = tables.second_name_element[self.roll(len(tables.second_name_element))]
+		name = first + second
+		if debug:
+			print('Name: ' + name)
+		return name
+
 	# handle logic for rolling for X number of features
-	#def roll_features():
-	#	num_features = self.roll(MAX_FEATURES)
-	#	features = [] 
-	#	i = 1
-	#	while i <= num_features:
-	#		k = self.roll(len(tables.features_roll_table))
-	#		if tables.features_roll_table[k] not in features:
-	#			features.append(tables.features_roll_table[k])
-	#			i += 1
-	#	return features
+	def roll_features(self, debug=False):
+		num_features = self.roll(MAX_FEATURES)
+		features = [] 
+		i = 1
+		while i <= num_features:
+			k = self.roll(len(tables.features_roll_table))
+			if tables.features_roll_table[k] not in features:
+				features.append(tables.features_roll_table[k])
+				i += 1
+
+		if debug:
+			print('Features: ' + str(features))
+		return features
 
 	def roll_resources(self, purpose, debug=False):
 		num_trade_resources = purpose[1]
@@ -49,6 +60,8 @@ class settlement:
 		if num_metal_resources > 0:
 			metal_roll = self.roll(len(tables.metal_roll_table))
 			resource_list.append(tables.metal_roll_table[metal_roll])
+		if debug:
+			print('Resources: ' + str(resource_list))
 		return resource_list
 
 	def roll_races(self, debug=False):
@@ -78,6 +91,9 @@ class settlement:
 					k += 1
 		else:
 			race_list.append(main_race)
+
+		if debug:
+			print('Races: ' + str(race_list))
 		return race_list
 
 	def roll_gods(self, debug=False):
@@ -98,42 +114,69 @@ class settlement:
 					k += 1
 		else:
 			god_list.append(main_god)
+
+		if debug:
+			print('Gods: ' + str(god_list))
 		return god_list
+
+	def printArray(self, arr):
+		x = len(arr)
+		y = len(arr) - 1
+		result = ""
+		for i in range(x):
+			if i == y:
+				result += ", and {}".format(arr[i])
+			else:
+				result += "{}, ".format(arr[i])
+
+
+	# convert the object to a nicely formatted string
+	def toPrettyString(self):
+		sentence = '{} is a {} of about {} people, located in {}. '.format(self.name, self.size, self.population, self.location) 
+		sentence += 'Its residents are: {}. The main deities worshipped here are {}. '.format(self.printArray(self.races), self.printArray(self.gods))
+		sentence += 'It was built as a {}, producing {}. It has a some noticable features, such as {}'.format(self.purpose, self.printArray(self.resources), self.printArray(self.features))
+		return sentence
+
+	def toCSVString(self):
+		return 'nothing for now'
 
 	# Generate a settlement and fill it in
 	def generate_settlement(self, debug=False):
-		name = "Some settlement"
-		if debug:
-			print('Name: ' + name)
-		#name = tables.first_name_element[self.roll(len(tables.first_name_element))] + tables.second_name_element[self.roll(len(tables.second_name_element))]
+		name = self.roll_name(debug)
+		features = self.roll_features(debug)
+		races = self.roll_races(debug)
+		gods = self.roll_gods(debug)
+		
 		size = tables.size_roll_table[self.roll(len(tables.size_roll_table))]
 		if debug:
 			print('size: ' + size)
+		
 		population_tuple = tables.population_roll_table[size]
 		population = (self.roll(population_tuple[0]) * population_tuple[1]) + population_tuple[2]
 		if debug:
 			print('Population: ' + str(population))
+		
 		location = tables.location_roll_table[self.roll(len(tables.location_roll_table))]
 		if debug:
 			print('Location: ' + location)
-		#features = self.roll_features(debug)
-		# if debug:
-		#	print('Features: ' + str(features))
+
 		purpose_roll = tables.purpose_roll_table[self.roll(len(tables.purpose_roll_table))]
 		purpose = purpose_roll[0]
 		if debug:
 			print('Purpose: ' + purpose)
 		resources = self.roll_resources(purpose_roll, debug)
-		if debug:
-			print('Resources: ' + str(resources))
-		races = self.roll_races(debug)
-		if debug:
-			print('Races: ' + str(races))
-		gods = self.roll_gods(debug)
-		if debug:
-			print('Gods: ' + str(gods))
 
-		return settlement(name, size, population, location, purpose, resources, races, gods)
+		self.name = name
+		self.features = features
+		self.races = races
+		self.gods = gods
+		self.population = population
+		self.location = location
+		self.purpose = purpose
+		self.resources = resources
+		self.size = size
+		return self
+		#return self.__init__(name, size, features, population, location, purpose, resources, races, gods)
 
 def __main__():
 	nargs = len(sys.argv)
@@ -141,14 +184,17 @@ def __main__():
 	if nargs > 2:
 		print("Error: There should not be any arguments passed via CLI")
 		return 1
-	
+
 	if nargs == 1:
 		debug = False
 	elif nargs == 2:
-		debug = sys.argv[1]
+		if sys.argv[1] == 'True':
+			debug = True
+		elif sys.argv[1] == 'False':
+			debug = False
 
 	# Create instance of settlement class for generator use
-	settlement_instance = settlement()
+	settlement_instance = Settlement()
 
 	# Ask for input (currently pseudo code)
 	num_settlements = int(raw_input())
@@ -166,8 +212,10 @@ def __main__():
 	else:
 		# 'range' is not inclusive of the end
 		for i in range(0, num_settlements):
-			print(settlement_instance.generate_settlement(debug))
-	exit()
+			print('--------NEW SETTLEMENT---------')
+			s = settlement_instance.generate_settlement(debug)
+			print(s.toPrettyString())
+	return 0
 
 
 __main__()
--- roll_tables.py
+++ roll_tables.py
@@ -1,15 +1,18 @@
 # Roll tables are NOT 0 indexed!
 # Range is not inclusive of the end, hence what seems to be overlap in indices
 
+# Fill up given roll_table from start index to end index (inclusive)
+# with the given value.
+def inc_range_fill(start, end, roll_table, val):
+	for i in range(start, end + 1):
+		roll_table[i] = val
+
 # Fill table for the size of the settlement (in terms of type)
 size_roll_table = {}
 size_roll_table[1] = 'Farm'
-for i in range(2,4):
-	size_roll_table[i] = 'Hamlet'
-for i in range(4,7):
-	size_roll_table[i] = 'Village'
-for i in range(6,8):
-	size_roll_table[i] = 'Town'
+inc_range_fill(2, 3, size_roll_table, 'Hamlet')
+inc_range_fill(4, 5, size_roll_table, 'Village')
+inc_range_fill(6, 7, size_roll_table, 'Town')
 size_roll_table[8] = 'City'
 
 # fill table for the racial composition of the settlement
@@ -40,8 +43,7 @@ elf_roll_table = {
 	5: 'Half-Elves',
 	6: 'Drow'
 }
-for i in range(1,4):
-	elf_roll_table[i] = 'Elves'
+inc_range_fill(1,3, elf_roll_table, 'Elves')
 
 # Fill table for orc type
 orc_roll_table = {
@@ -69,15 +71,10 @@ location_roll_table = {
 # Example:
 # Natural Resources, 2 trade resources, 0 metal resources
 purpose_roll_table = {}	
-for i in range (1,4):
-	purpose_roll_table[i] = ('Road/River convergence', 1, 0)
-for i in range (4,7):
-	purpose_roll_table[i] = ('Natural Resource', 2, 0)
-for i in range (7,9):
-	purpose_roll_table[i] = ('Mining Settlement', 1, 1)
-for i in range (9,11):
-	purpose_roll_table[i] = ('Military/Strategic Value', 0, 0)
-
+inc_range_fill(1, 3, purpose_roll_table, ('Road/River convergence', 1, 0))
+inc_range_fill(4, 6, purpose_roll_table, ('Natural Resources', 2, 0))
+inc_range_fill(7, 8, purpose_roll_table, ('Mining Settlement', 1, 1))
+inc_range_fill(9, 10, purpose_roll_table, ('Military/Strategic Value', 0, 0))
 
 # Fill metal resource table
 metal_roll_table = {
@@ -101,8 +98,7 @@ trade_resource_roll_table = {
 	9: 'Natural Oils or Fuels',
 	10: 'Rare Animals'
 }
-for i in range(1,5):
-	trade_resource_roll_table[i] = 'Grazing Land'
+inc_range_fill(1, 4, trade_resource_roll_table, 'Grazing Land')
 
 
 # Population formula
@@ -137,3 +133,196 @@ gods_roll_table = {
 	17: '2-Mix',
 	18: '3-Mix'
 }
+
+features_roll_table = {
+	1: 'Arms',
+	2: 'Books',
+	3: 'Brewing',
+	4: 'Centre of Learning',
+	5: 'Crumbling ramparts/palisade',
+	6: 'Eerie ruin nearby',
+	7: 'Famous for annual feast or festival',
+	8: 'Famous/notorious inn',
+	9: 'Foodstuffs',
+	10: 'Glass or pottery crafters',
+	11: 'Goats',
+	12: 'Government',
+	13: 'Home of famous artist, actor, or author',
+	14: 'Horse breeding',
+	15: 'Illicit services',
+	16: 'Imortant argricultural producer',
+	17: 'Imposing bridge or other construction',
+	18: 'Impressive shrine, statue, or memorial',
+	19: 'Impressive temple to major deity',
+	20: 'Inhabitants ostracized by outsiders',
+	21: 'Known as lawless place',
+	22: 'Large dwarven community (2d10%)',
+	23: 'Large library',
+	24: 'Large producer of charcoal',
+	25: 'Natural fort',
+	26: 'Logging, timber, and sawing',
+	27: 'Lush park, gardens, or commons',
+	28: 'Major road/river warden station',
+	29: 'Mentioned in famous song, saga, or poem',
+	30: 'Mighty watch tower or beacon',
+	31: 'Military strongoiint (double garrison)',
+	32: 'Mining (metal or mineral)',
+	33: 'Notorious for corrupt officials',
+	34: 'On the decline',
+	35: 'Partially in ruins',
+	36: 'Preserves',
+	37: 'Recently under attack',
+	38: 'Rumored site of unnatural events',
+	39: 'Recently prospering',
+	40: 'Geographically isolated',
+	41: 'Historical importance',
+	42: 'Sheep',
+	43: 'Significant center of trade',
+	44: 'Significant fortifications',
+	45: 'Major prison/labor camp site',
+	46: 'Recent unrest',
+	47: 'Sizable Halfling Community (2d10%)',
+	48: 'Greedy Tradesmen',
+	49: 'Trapping',
+	50: 'Wine'
+}
+
+
+first_name_element = {
+	1: 'All',
+	2: 'Als',
+	3: 'Alt',
+	4: 'Ammer',
+	5: 'Arn',
+	6: 'Aschen',
+	7: 'Ascher',
+	8: 'Bens',
+	9: 'Birn',
+	10: 'Blanken',
+	11: 'Blau',
+	12: 'Blut',
+	13: 'Bogen',
+	14: 'Borken',
+	15: 'Braun',
+	16: 'Buche',
+	17: 'Diep',
+	18: 'Dunkel',
+	19: 'Eis',
+	20: 'Ell',
+	21: 'Eschen',
+	22: 'Fried',
+	23: 'Fursten',
+	24: 'Gels',
+	25: 'Gelb',
+	26: 'Giessen',
+	27: 'Grab',
+	28: 'Graten',
+	29: 'Grau',
+	30: 'Gros',
+	31: 'Gruft',
+	32: 'Grun',
+	33: 'Gunz',
+	34: 'Hagel',
+	35: 'Hall',
+	36: 'Hart',
+	37: 'Hauzen',
+	38: 'Hech',
+	39: 'Herbst',
+	40: 'Herz',
+	41: 'Hohen',
+	42: 'Holz',
+	43: 'Ilmen',
+	44: 'Kall',
+	45: 'Klage',
+	46: 'Klein',
+	47: 'Lauffen',
+	48: 'Lauter',
+	49: 'Lipp',
+	50: 'Main',
+	51: 'Marien',
+	52: 'Merse',
+	53: 'Mess',
+	54: 'Moos',
+	55: 'Naum',
+	56: 'Nenn',
+	57: 'Nieder',
+	58: 'Nord',
+	59: 'Ober',
+	60: 'Ost',
+	61: 'Oster',
+	62: 'Otters',
+	63: 'Pfaffen',
+	64: 'Pfarr',
+	65: 'Pfeffen',
+	66: 'Rain',
+	67: 'Riesen',
+	68: 'Saal',
+	69: 'Salz',
+	70: 'Schaf',
+	71: 'Schone',
+	72: 'Schroben',
+	73: 'Schwab',
+	74: 'Schwan',
+	75: 'Schwarz',
+	76: 'Semmen',
+	77: 'Sim',
+	78: 'Sommer',
+	79: 'Sonder',
+	80: 'Speeger',
+	81: 'Thurin',
+	82: 'Tier',
+	83: 'Tranen',
+	84: 'Trost',
+	85: 'Upfen',
+	86: 'Vater',
+	87: 'Vecker',
+	88: 'Wall',
+	89: 'Waren',
+	90: 'Wein',
+	91: 'Weiss',
+	92: 'West',
+	93: 'Winter',
+	94: '[charname]',
+	95: 'Starn',
+	96: 'Stein',
+	97: 'Sud',
+	98: 'Tauber',
+	99: 'Taufel'
+}
+
+second_name_element = {}
+inc_range_fill(1, 3, second_name_element, 'acker')
+inc_range_fill(4, 6, second_name_element, 'bach')
+inc_range_fill(7, 9, second_name_element, 'berg')
+inc_range_fill(10, 12, second_name_element, 'brucke')
+inc_range_fill(13, 15, second_name_element, 'brunnen')
+inc_range_fill(16, 18, second_name_element, 'burg')
+inc_range_fill(19, 21, second_name_element, 'chen')
+inc_range_fill(22, 24, second_name_element, 'dorf')
+inc_range_fill(25, 27, second_name_element, 'feld')
+inc_range_fill(28, 30, second_name_element, 'fort')
+inc_range_fill(31, 33, second_name_element, 'galgen')
+inc_range_fill(34, 36, second_name_element, 'haus')
+inc_range_fill(37, 39, second_name_element, 'hausen')
+inc_range_fill(40, 42, second_name_element, 'helde')
+inc_range_fill(43, 45, second_name_element, 'helm')
+inc_range_fill(46, 48, second_name_element, 'hof')
+inc_range_fill(49, 51, second_name_element, 'holz')
+inc_range_fill(52, 54, second_name_element, 'horst')
+inc_range_fill(55, 57, second_name_element, 'hugel')
+inc_range_fill(58, 60, second_name_element, 'hut')
+inc_range_fill(61, 63, second_name_element, 'hutte')
+inc_range_fill(64, 66, second_name_element, 'leben')
+inc_range_fill(67, 69, second_name_element, 'munde')
+inc_range_fill(70, 72, second_name_element, 'schweig')
+inc_range_fill(73, 75, second_name_element, 'stadt')
+inc_range_fill(76, 78, second_name_element, 'statte')
+inc_range_fill(79, 81, second_name_element, 'stein')
+inc_range_fill(82, 84, second_name_element, 'tur')
+inc_range_fill(85, 87, second_name_element, 'wald')
+inc_range_fill(88, 90, second_name_element, 'wasser')
+inc_range_fill(91, 92, second_name_element, 'alte')
+inc_range_fill(93, 94, second_name_element, 'bad')
+inc_range_fill(95, 96, second_name_element, 'grosse')
+inc_range_fill(97, 98, second_name_element, 'hellige')
+inc_range_fill(99, 100, second_name_element, 'neue')
